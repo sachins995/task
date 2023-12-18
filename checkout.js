4cbfb06d4d9c5e33f7c3ee8f276b154ccf6672ef
sachins995 <sachin.psl99@gmail.com>
2023-12-18 18:59:19 +0530

    Commited

---
+++ checkout.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', function() { 
+document.getElementById("radioButton2").addEventListener("click", function(){
+let x = document.getElementById("checkout2")
+x.style.backgroundColor = "rgba(0, 127, 97, 1)"
+// document.getElementById("radioButton2").style.backgroundColor = "rgba(0, 127, 97, 1)"
+
+
+})
+
+// document.getElementById("radioButton2").addEventListener("click", function(){
+//     let x = document.getElementById("checkout2")
+//     x.style.backgroundColor = "rgba(0, 127, 97, 1)"
+
+
+// })
+
+})
